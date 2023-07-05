352cf966e1985f05e524711b8d95a37e19bab2c3
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-07-04 18:24:25 -0600

    implemented .env variables for development and production

--- src/api/axios_base.ts
+++ src/api/axios_base.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-
 export const axios_base = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL_DEV : import.meta.env.VITE_API_URL_PROD,
     headers: {
         'Content-Type': 'application/json'
     }
--- src/pages/auth/actions/login.ts
+++ src/pages/auth/actions/login.ts
@@ -1,16 +1,18 @@
 import { axios_base } from '../../../api/axios_base'
 
+export const host = window.location.origin;
+
 export const onGoogleAuthenticate = async () => {
-    const resp = await axios_base.get('http://127.0.0.1:8000/auth/o/google-oauth2/?redirect_uri=http://127.0.0.1:5173/auth/oauth2/google/')
+    const resp = await axios_base.get(`auth/o/google-oauth2/?redirect_uri=${host}/auth/oauth2/google/`)
     window.location.replace(resp.data.authorization_url)
 }
 
 export const onLinkedinAuthenticate = async () => {
-    const resp = await axios_base.get('http://127.0.0.1:8000/auth/o/linkedin-oauth2/?redirect_uri=http://127.0.0.1:5173/auth/oauth2/linkedin/')
+    const resp = await axios_base.get(`auth/o/linkedin-oauth2/?redirect_uri=${host}/auth/oauth2/linkedin/`)
     window.location.replace(resp.data.authorization_url)
 }
 
 export const onGithubAuthenticate = async () => {
-    const resp = await axios_base.get('http://127.0.0.1:8000/auth/o/github/?redirect_uri=http://127.0.0.1:5173/auth/oauth2/github/')
+    const resp = await axios_base.get(`auth/o/github/?redirect_uri=${host}/auth/oauth2/github/`)
     window.location.replace(resp.data.authorization_url)
 }
