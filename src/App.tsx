99dbbf84129a7c3209855eeb796c1847439dafeb
kevfdez <kevin.fernandez4579@alumnos.udg.mx>
2023-07-16 10:03:32 -0600

    Implement NavBar v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from './pages/home/NavBar'
 import { AppRouter } from './routes/AppRouter'
 
 
 export const App = () => {
-  
+
+    const currentPage = window.location.pathname;
+    const excludedPages = ['/login', '/register'];
+    const isNavBarShown = !excludedPages.includes(currentPage);
+
     return (
-        <AppRouter />
+        <>
+            {isNavBarShown && <NavBar />}
+            <AppRouter />
+        </>
     )
 }
 
