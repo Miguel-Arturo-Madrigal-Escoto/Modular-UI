95383028b6fa134f1751b4000a5824588f4546c4
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-07-30 13:55:19 -0600

    company roles saved in db

--- src/app/roles/rolesSlice.ts
+++ src/app/roles/rolesSlice.ts
@@ -1,17 +1,27 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { onAddCompanyRoles, onGetCompanyRoles } from './thunks';
+import { ICompanyRolesById } from '../../pages/auth/types/interfaces';
 
 export interface Role {
-    id: string;
+    id?: string;
+    name: string;
+    link: string;
     position: string;
     description: string;
 }
 
 export interface RolesState {
     roles: Role[];
+    errors: any;
+    loading: boolean;
+    company_roles: ICompanyRolesById[];
 }
 
 export const initialState: RolesState = {
-    roles: []
+    roles: [],
+    errors: {},
+    loading: false,
+    company_roles: []
 }
 
 export const rolesSlice = createSlice({
@@ -36,9 +46,74 @@ export const rolesSlice = createSlice({
                 roles: state.roles.map(role => role.id === payload.id ? payload : role)
             }
         },
+        clearRoles: (state) => {
+            return {
+                ...state,
+                roles: []
+            }
+        },
+        setErrors: (state, { payload }) => {
+            return {
+              ...state,
+              errors: payload
+            }
+        }
     },
+    extraReducers(builder) {
+        builder.addCase(onAddCompanyRoles.pending, (state) => {
+          return {
+            ...state,
+            loading: true,
+            errors: {},
+          }
+        })
+  
+        // 
+        builder.addCase(onAddCompanyRoles.fulfilled, (state, { payload }) => {
+          return {
+            ...state,
+            errors: {},
+            loading: false
+          }
+        }),
+  
+        // 
+        builder.addCase(onAddCompanyRoles.rejected, (state) => {
+          return {
+            ...state,
+            loading: false
+          }
+        })
+
+
+        builder.addCase(onGetCompanyRoles.pending, (state) => {
+            return {
+              ...state,
+              loading: true,
+              errors: {},
+            }
+          })
+    
+          // 
+          builder.addCase(onGetCompanyRoles.fulfilled, (state, { payload }) => {
+            return {
+              ...state,
+              errors: {},
+              loading: false,
+              company_roles: payload
+            }
+          }),
+    
+          // 
+          builder.addCase(onGetCompanyRoles.rejected, (state) => {
+            return {
+              ...state,
+              loading: false
+            }
+          })
+    }
 })
 
-export const { addRole, removeRole, editRole } = rolesSlice.actions;
+export const { addRole, removeRole, editRole, clearRoles, setErrors } = rolesSlice.actions;
 
 export default rolesSlice.reducer;
--- src/pages/home/NavBar.tsx
+++ src/pages/home/NavBar.tsx
@@ -57,7 +57,7 @@ export const NavBar = () => {
                             <li>
                                     <a
                                         className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-left text-black-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-black-700 dark:hover:bg-neutral-100 focus:bg-indigo-100 focus:text-indigo-500"
-                                        href="/"
+                                        href="/for-you"
                                         data-te-dropdown-item-ref
                                     >
                                         Home
