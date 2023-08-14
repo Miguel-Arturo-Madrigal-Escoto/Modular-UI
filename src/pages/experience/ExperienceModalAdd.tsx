8241d3d867d15ca26f1795a8ad9b9b0ae45a43a7
kevfdez <kevin.fernandez4579@alumnos.udg.mx>
2023-08-13 20:07:02 -0600

    Add validation error messages when user adds a new experience.

--- src/pages/experience/ExperienceModalAdd.tsx
+++ src/pages/experience/ExperienceModalAdd.tsx
@@ -4,6 +4,7 @@ import { setModalClosedExperience } from "../../app/extra/modalSlice";
 import { IExperienceAddForm } from "./types/interfaces";
 import { TitleForm } from "../auth/TitleForm";
 import { onAddUserExperience, onGetUserExperiences } from "../../app/experience/thunks";
+import { FormErrorMessage } from "../../components/auth/FormErrorMessage";
 
 
 export const ExperienceModalAdd = () => {
@@ -14,12 +15,10 @@ export const ExperienceModalAdd = () => {
     } = useForm<IExperienceAddForm>();
     const { user_data } = useAppSelector(state => state.auth);
     const { positions } = useAppSelector(state => state.form);
+    const { errors } = useAppSelector(state => state.experience);
 
     const dispatch = useAppDispatch();
 
-    console.log(user_data)
-
-    
     const onSubmit: SubmitHandler<IExperienceAddForm> = async data => {
         try {
             await dispatch(onAddUserExperience({
@@ -46,21 +45,21 @@ export const ExperienceModalAdd = () => {
                             <label className="ml-3 text-sm font-bold text-gray-700 tracking-wide">Fecha de inicio</label>
                             <input
                                 className=" w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-indigo-500"
-                                type="date" placeholder="Ingresa el nombre del puesto" {...register('start_date')}
+                                type="date"{...register('start_date')}
                             />
-                            {/* {
-                                errors.name && <FormErrorMessage message={errors.name} />
-                            } */}
+                            {
+                                errors.start_date && <FormErrorMessage message={errors.start_date} />
+                            }
                         </div>
                         <div className="relative">
                             <label className="ml-3 text-sm font-bold text-gray-700 tracking-wide">Fecha de finalizacion</label>
                             <input
                                 className=" w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-indigo-500"
-                                type="date" placeholder="Ingresa el link de tu empresa" {...register('end_date')}
+                                type="date" {...register('end_date')}
                             />
-                            {/* {
-                                errors.name && <FormErrorMessage message={errors.name} />
-                            } */}
+                            {
+                                errors.end_date && <FormErrorMessage message={errors.end_date} />
+                            }
                         </div>
                         <div className="relative">
                             <label className="ml-3 text-sm font-bold text-gray-700 tracking-wide">Rol</label>
@@ -75,9 +74,9 @@ export const ExperienceModalAdd = () => {
                         <div className="relative">
                             <label className="ml-3 text-sm font-bold text-gray-700 tracking-wide">Descripción</label>
                             <textarea rows={4} cols={40} className=" w-full text-base px-4 border border-gray-300 focus:outline-none  resize-none focus:border-indigo-500" {...register('description')}/>
-                            {/* {
+                            {
                                 errors.description && <FormErrorMessage message={ errors.description }/>
-                            } */}
+                            }
                         </div>
                     </div>
                     <div className="relative right-0">
