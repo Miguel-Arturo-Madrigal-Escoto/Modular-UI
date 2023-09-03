45e80d40db4b6d7bb0615616fd67612967119924
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-09-03 14:14:52 -0600

    upper case for location

--- src/pages/matches/CompanyMatch.tsx
+++ src/pages/matches/CompanyMatch.tsx
@@ -25,8 +25,8 @@ export const CompanyMatch: FC<Props> = ({ user}) => {
             <div className="flex flex-col">
                 <div className="bg-white border border-white shadow-lg  rounded-3xl p-4 m-4">
                     <div className="flex-none sm:flex">
-                        <div className=" relative h-32 w-32   sm:mb-0 mb-3">
-                            <img src={ user.image || defaultImageProfile } alt="Imagen del match (user)" className=" w-32 h-32 object-cover rounded-2xl" />
+                        <div className=" relative w-40 h-40   sm:mb-0 mb-3">
+                            <img src={ user.image || defaultImageProfile } alt="Imagen del match (user)" className="w-40 h-40 object-cover rounded-2xl" />
                         </div>
                         <div className="flex-auto sm:ml-5 justify-evenly">
                             <div className="flex items-center justify-between sm:mt-2">
@@ -34,7 +34,9 @@ export const CompanyMatch: FC<Props> = ({ user}) => {
                                     <div className="flex flex-col">
                                         <div className="w-full flex-none text-lg text-gray-800 font-bold leading-none">{ `${ user.name } ${ user.lastname }` }</div>
                                         <div className="flex-auto text-gray-500 my-1">
-                                            <span className="mr-3 ">{ positions.find(p => p.id === user.position)?.display }</span><span className="mr-3 border-r border-gray-200  max-h-0"></span><span>{ user.location }</span>
+                                            <span className="mr-3 ">{ positions.find(p => p.id === user.position)?.display }</span>
+                                            <span className="mr-3 border-r border-gray-200 max-h-0"></span>
+                                            <span className="uppercase">{ user.location }</span>
                                         </div>
                                     </div>
                                 </div>
@@ -44,7 +46,7 @@ export const CompanyMatch: FC<Props> = ({ user}) => {
                                     <span className="mr-3 ">Salario esperado: { user.expected_salary }</span>
                                 </div>
                                 <div className="flex-auto text-gray-500 my-1">
-                                    <span className="mr-3 ">{ user.about }</span>
+                                    <span className="mr-3 capitalize">{ user.about }</span>
                                 </div>
                             </div>
                             <div className="flex pt-2  text-sm text-gray-500">
--- src/pages/matches/UserMatch.tsx
+++ src/pages/matches/UserMatch.tsx
@@ -25,23 +25,23 @@ export const UserMatch: FC<Props> = ({ company }) => {
             <div className="flex flex-col">
                 <div className="bg-white border border-white shadow-lg  rounded-3xl p-4 m-4">
                     <div className="flex-none sm:flex">
-                        <div className=" relative h-32 w-32   sm:mb-0 mb-3">
-                            <img src={ company.image || defaultImageProfile } alt="Imagen del match (company)" className=" w-32 h-32 object-cover rounded-2xl" />
+                        <div className=" relative w-40 h-40   sm:mb-0 mb-3">
+                            <img src={ company.image || defaultImageProfile } alt="Imagen del match (company)" className="w-40 h-40 object-cover rounded-2xl" />
                         </div>
                         <div className="flex-auto sm:ml-5 justify-evenly">
                             <div className="flex items-center justify-between sm:mt-2">
                                 <div className="flex items-center">
                                     <div className="flex flex-col">
-                                        <div className="w-full flex-none text-lg text-gray-800 font-bold leading-none">{ company.name }</div>
+                                        <div className="w-full flex-none text-lg text-gray-800 font-bold leading-none capitalize">{ company.name }</div>
                                         <div className="flex-auto text-gray-500 my-1">
-                                            <span className="mr-3 ">{ sectors.find(s => s.id === company.sector)?.display }</span><span className="mr-3 border-r border-gray-200  max-h-0"></span><span>{ company.location }</span>
+                                            <span className="mr-3 ">{ sectors.find(s => s.id === company.sector)?.display }</span><span className="mr-3 border-r border-gray-200 max-h-0"></span><span className="uppercase">{company.location}</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="flex flex-row items-center">
                                 <div className="flex-auto text-gray-500 my-1">
-                                    <span className="mr-3 ">{ company.about }</span>
+                                    <span className="mr-3 capitalize">{ company.about }</span>
                                 </div>
                             </div>
                             <div className="flex pt-2  text-sm text-gray-500">
