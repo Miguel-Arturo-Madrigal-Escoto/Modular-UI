748317f96b5512fdb3f4d7b923191713c9655ebf
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-11-05 23:41:35 -0600

    removed condition in savelocalstorage

--- src/app/helpers/saveLocalStorageState.ts
+++ src/app/helpers/saveLocalStorageState.ts
@@ -4,8 +4,5 @@ export const saveLocalStorageState = (state: object) => {
         if (value) {
             localStorage.setItem(key, value);
         }
-        else if (!value && localStorage.getItem(key)) {
-            localStorage.removeItem(key);
-        }
     })
 }
